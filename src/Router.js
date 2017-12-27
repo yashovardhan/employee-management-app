57f9be56dad01ba71af6c5fb8305725040e4cfee
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-27 13:32:07 +0530

    Add Employee Form

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{ paddingTop: 65 }}>
+    <Router sceneStyle={{ paddingTop: 65 }} initial>
       <Scene key="auth">
-        <Scene key="login" component={LoginForm} title="Manager - Login" />
+        <Scene key="login" component={LoginForm} title="Login" />
       </Scene>
 
       <Scene key="main">
-        <Scene key="employeeList" component={EmployeeList} title="Manager - Employees" />
+        <Scene
+          rightTitle="Add"
+          onRight={() => Actions.employeeCreate()}
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          initial
+        />
+      <Scene key="employeeCreate" component={EmployeeCreate} title="Add Employee" />
       </Scene>
     </Router>
   );
---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,20 @@
+import {
+  EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+        // action.payload === { prop: 'name', value: 'jane' }
+      return { ...state, [action.payload.prop]: action.payload.value };
+
+    default:
+      return state;
+  }
+};
