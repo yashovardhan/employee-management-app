28d5d14c7e27165db55527dad3c44f20c7dafbf6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-27 20:38:41 +0530

    Fix wrong placement of initial

--- src/Router.js
+++ src/Router.js
@@ -6,8 +6,8 @@ import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{ paddingTop: 65 }} initial>
-      <Scene key="auth">
+    <Router sceneStyle={{ paddingTop: 65 }}>
+      <Scene key="auth" initial>
         <Scene key="login" component={LoginForm} title="Login" />
       </Scene>
 
