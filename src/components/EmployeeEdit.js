5bf8fcf71ce8401fa5b37ff0f94f5eb4c69f20b4
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-28 18:52:12 +0530

    Add Text Employee feature

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
@@ -18,13 +19,26 @@ class EmployeeEdit extends Component {
     this.props.employeeSave({ name, phone, shift, uid: employee.uid });
   }
 
+  onTextPress() {
+    const { name, phone, shift } = this.props;
+
+    Communications.text(phone, `${name}, you upcoming shift is on ${shift}`);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Save
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
       </Card>
